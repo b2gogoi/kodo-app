1fb1edd3e408d2a78a05673f3cd5ac67fa899a08
Bhaskar Gogoi <b2gogoi@gmail.com>
2021-08-27 15:22:17 +0530

    refactor trim not required

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -5,7 +5,7 @@ export default function SearchBar({filter, text}: props) {
     const [search, setSearch] = useState(text);
 
     useEffect(() => {
-        setSearch(text.trim());
+        setSearch(text);
     }, [text])
     return (<form className="search-box">
         <input type="search" value={search} placeholder="Search in name or description" onChange={(e) => {
