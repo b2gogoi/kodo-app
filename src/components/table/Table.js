7d581bc1c7dd07c5805738704ccf09d86587ba00
Bhaskar Gogoi <b2gogoi@gmail.com>
2021-09-08 18:59:49 +0530

    show all columns even in mobile,  table display changed from flex to grid

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -13,19 +13,19 @@ export default function Table({data, colOrderSeq, sortCol, displayHeaderMap}) {
     return (<>
     <Pagination items={allData} callback={setPageData} />
     <div className="table-container">
-        <div className="table-headers-row">
-            {colOrderSeq.map((col, i) => <div key={col} className={sortCol === col ? `sorted column-${col}` : `column-${col}`}>
+        <div className="table-grid-header-container">
+            {colOrderSeq.map((col, i) => <div key={col} className={sortCol === col ? `sorted column-headers-${col}` : `column-headers-${col}`}>
                 {displayHeaderMap[col] ? displayHeaderMap[col] : col.replace(/([A-Z])/g, ' $1')}
             </div>)}
         </div>
-        <div className="table-body-container">
-            {pageData.map((row, i) => <div key={`${row.name}-${i}`} className="table-row">
-                {colOrderSeq.map((col, i) => <div key={`${col}-${i}`} className={`column-${col}`}>
+        <div className="table-grid-container">
+            {pageData.map((row, i) => {
+                return colOrderSeq.map((col, i) => <div key={`${col}-${i}`} className={`column-data-${col}`}>
                     {col === 'image' ? 
                         <img src={row[col]} alt={row.name} />
                         : col !== 'dateLastEdited' ? row[col] : format(row[col])}
-                </div>)}
-            </div>)}
+                </div>);
+            })}
         </div>
     </div>
     </>)
